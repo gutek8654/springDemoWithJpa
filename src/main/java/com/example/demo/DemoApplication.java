0ea451437e8009efa4d1d150ca5f8809b5097a97
Kamil Krzeminski <kamil.krzeminski@gft.com>
2019-03-28 16:37:14 +0100

    cleanup and removing EnableJpaRepositories annotation to fix IT

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+//@EnableJpaRepositories
 public class DemoApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
